a8c594f8b5f3ffd4cd6ceabc0ebdffa11938a090
juantiradomt <juan.tirado.mt@correo.usa.edu.co>
2021-10-13 21:37:09 -0500

    Primer proyecto Spingboot

---
+++ src/test/java/grupo32octubre13/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo32octubre13.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
